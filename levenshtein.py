0e9e99d5facbd921fb464e301599f71fc80c8278
Kazuki Kawamura <kkawamura@ai.cs.kobe-u.ac.jp>
2019-11-20 21:20:48 +0900

    class2method

--- levenshtein.py
+++ levenshtein.py
@@ -1,32 +1,23 @@
-class Levenshtein:
+def LevenshteinDistance(str1,str2):
+    '''
+    distance[i][j] : str1のi番目までの文字列とstr2のj番目までの文字列の編集距離
+    '''
+    distance = [[0] * (len(str2)+1)] * (len(str1)+1)
+    for i in range(len(str1)+1):
+        distance[i][0] = i
+    for j in range(len(str2)+1):
+        distance[0][j] = j
 
-    def initArray(self,str1,str2):
-        distance = []
-        for i in range(len(str1)+1):
-            distance.append([0]*(len(str2)+1))
-            distance[i][0] = i
-        for j in range(len(str2)+1):
-            distance[0][j] = j
-        return distance
+    for i in range(1,len(str1)+1):
+        for j in range(1,len(str2)+1):
+            d_1 = distance[i-1][j-1] if str1[i-1]==str2[j-1] else distance[i-1][j-1]+1
+            d_2 = distance[i][j-1]+1
+            d_3 = distance[i-1][j]+1
+            distance[i][j]=min([d_1, d_2, d_3])
 
-    def editDist(self,str1,str2,distance):
-        dist = [0]*3
-        for i in range(1,len(str1)+1):
-            for j in range(1,len(str2)+1):
-                dist[0] = distance[i-1][j-1] if str1[i-1]==str2[j-1] else distance[i-1][j-1]+1
-                dist[1] = distance[i][j-1]+1
-                dist[2] = distance[i-1][j]+1
-                distance[i][j]=min(dist)
-        return distance[i][j]
-
-    def __init__(self,str1,str2):
-        self.str1 = str1
-        self.str2 = str2
-        Levenshtein.distance = self.initArray(str1,str2)
-        Levenshtein.dist = self.editDist(str1,str2,Levenshtein.distance)
+    return distance[i][j]
 
 if __name__ == '__main__':
     str1 = 'coffee'
     str2 = 'cafe'
-    leven = Levenshtein(str1,str2)
-    print(leven.dist)
+    print(LevenshteinDistance(str1,str2))
