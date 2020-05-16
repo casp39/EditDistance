22218e8b470566ebd4dc05a9b9cfd6ff87a07d28
Kazuki Kawamura <electric511casp@gmail.com>
2020-05-17 08:16:10 +0900

    mv tree_edit_distance old

--- tree_edit_distance_.py
+++ tree_edit_distance_.py
@@ -1,5 +1,29 @@
 from anytree import Node, RenderTree
 
+class Forest(object):
+    def __init__(self, forest):
+        self.forest = forest
+
+    def rightmost_forest(self):
+        return f[-1]
+
+    def count(self):
+        return self.r - self.l
+
+    def empty(self):
+        return self.l == self.r
+
+class ForestEditDistance(object):
+    def __init__(self, f1, f2):
+        self.f1, self.f2 = f1, f2
+        self.d = []
+
+    def edit_distance(self):
+        if xforest.empty: return yforest.count
+        if yforest.empty: return xforest.count
+
+        xrtree = xforest.rightmost_forest
+        yrtree = yforest.rightmost_forest
 
 
 if __name__ == '__main__':
